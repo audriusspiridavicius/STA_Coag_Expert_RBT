c8521cb0d89f2612cbfcccb33dc83fd9ce8283a2
Audrius <audriusspiridavicius@gmail.com>
2025-04-14 18:37:12 +0300

    show only to be validated status samples

--- dashboard_page.py
+++ dashboard_page.py
@@ -21,7 +21,28 @@ class DashboardPage:
         self.home_button = self.dashboard.child_window(control_type="Pane", auto_id="btnHome")
         self.samples_to_be_validated = []
         self.refresh_button.click_input()
+
+        self.pending_checkbox = self.dashboard.child_window(control_type="CheckBox", title="Pending")
+        self.rerun_checkbox = self.dashboard.child_window(control_type="CheckBox", title="Selected To Be Rerun")
+        self.validated_checkbox = self.dashboard.child_window(control_type="CheckBox", title="Validated")
+
+        # uncheck the pending checkbox if it is checked
+        if self.pending_checkbox.get_toggle_state() == 1:
+            self.pending_checkbox.toggle()
+            print("Pending checkbox unchecked")
+
+        # uncheck rerun checkbox if it is checked
+        if self.rerun_checkbox.get_toggle_state() == 1:
+            self.rerun_checkbox.toggle()
+            print("Rerun checkbox unchecked")
         
+        # uncheck validated checkbox if it is checked
+        if self.validated_checkbox.get_toggle_state() == 1:
+            self.validated_checkbox.toggle()
+            print("Validated checkbox unchecked")
+
+
+
     def get_sample_status(self) -> None:
         """
         Get the status of the sample.
@@ -69,3 +90,11 @@ class DashboardPage:
             else:
                 print(f"Sample {sample} is not visible")
 
+        if self.pending_checkbox.get_toggle_state() == 0:
+            self.pending_checkbox.toggle()
+
+        if self.rerun_checkbox.get_toggle_state() == 0:
+            self.rerun_checkbox.toggle()
+
+        if self.validated_checkbox.get_toggle_state() == 0:
+            self.validated_checkbox.toggle()
